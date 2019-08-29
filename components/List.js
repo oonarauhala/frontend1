b36430a062b7102f887f949a43fa260802594084
Oona <oona.e.rauhala@gmail.com>
2019-08-29 14:21:57 +0300

    Ajax + state: Task A

--- components/List.js
+++ components/List.js
@@ -1,16 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {
   FlatList,
 } from 'react-native';
 import ListItem from './ListItem';
+import {MediaContext} from '../contexts/MediaContexts';
+
 
 const List = (props) => {
-  console.log(props);
+  const [media, setMedia] = useContext(MediaContext);
+
+  const getMedia = () => {
+    fetch(`https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((result) => {
+          console.log(result);
+          setMedia(result);
+        });
+  };
+
+  useEffect(() => getMedia(), []);
+
   return (
     <FlatList
-      data={props.mediaArray}
+      data={media}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     />
   );
